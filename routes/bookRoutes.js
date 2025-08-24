f2a38d18448cc7f521f2f9e4528902219a647618
Sintja <sporeci2000@gmail.com>
2025-08-24 13:13:13 -0400

    Final

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,7 @@
+// Import Express so we can make a router
 const express = require('express');
 
+// Imports the Mongoose model
 const Book = require('../models/Book');
 
 // Create a new router instance
@@ -8,6 +10,8 @@ const router = express.Router();
 // Create a new book
 router.post('/', async (req, res) => {
     try {
+
+        // req.body contains the data sent by the client
         const newBook = await Book.create(req.body);
         res.status(201).json(newBook);
     } catch (err) {
@@ -16,7 +20,7 @@ router.post('/', async (req, res) => {
 });
 
 // Get all books
-router.get('/', async (_req, res) => {
+router.get('/', async (req, res) => {
     try {
         const books = await Book.find();
         res.json(books);
@@ -28,10 +32,12 @@ router.get('/', async (_req, res) => {
 // Get one book by ID
 router.get('/:id', async (req, res) => {
     try {
+        // Queries MongoDB for a document with that ID
         const book = await Book.findById(req.params.id);
-        if (!book) return res.status(404).json({ error: 'Book not found' });
+        if (!book) return res.status(404).json({ error: 'Error! Book not found...' });
         res.json(book);
     } catch (err) {
+        // Internal Server Error.
         res.status(500).json({ error: err.message });
     }
 });
@@ -40,12 +46,17 @@ router.get('/:id', async (req, res) => {
 router.put('/:id', async (req, res) => {
     try {
         const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+            // Returns the updated document instead of the old one
             new: true,
+
+            // Ensures Mongoose validates the data before updating
             runValidators: true
         });
+        // Not Found
         if (!updatedBook) return res.status(404).json({ error: 'Book not found' });
         res.json(updatedBook);
     } catch (err) {
+        // Bad Request
         res.status(400).json({ error: err.message });
     }
 });
